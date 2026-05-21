e4a1c3b401e17560a1bc47d195037389b6bbcdda
Jtoast65 <vikings1155@gmail.com>
2026-05-21 14:18:58 -0700

    Update global.js

--- global.js
+++ global.js
@@ -7,7 +7,7 @@ function $$(selector, context = document) {
 const BASE_PATH =
   location.hostname === "localhost" || location.hostname === "127.0.0.1"
     ? "/"
-    : "/dsc106portfolio/";
+    : "/portfolio/";
 
 let pages = [
   { url: "", title: "Home" },
